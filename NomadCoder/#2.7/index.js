c607af36b7c449516067ef7019b05af036d5fe9d
Hyorin Kim <hyoriinvic@gmail.com>
2020-09-16 17:52:11 +0900

    update

--- NomadCoder/#2.7/index.js
+++ NomadCoder/#2.7/index.js
@@ -3,7 +3,7 @@
 const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
-funciton handleClick(){
+funciton handleClick() {
     const currentClass = title.className;
     if(currentClass !== CLICKED_CLASS) {
         title.className = CLICKED_CLASS;
@@ -15,4 +15,5 @@ funciton handleClick(){
 function init() {
     title.addEventListener("click", handleClick);
 }
+
 init();
