6fb99fcee0fa1124101750ad378830e546d6d31a
Hyorin Kim <hyoriinvic@gmail.com>
2020-09-22 19:13:20 +0900

    update

--- NomadCoder/#3 JS APP/clock.js
+++ NomadCoder/#3 JS APP/clock.js
@@ -1,20 +1,24 @@
+// .querySelector: element 의 자식(child) 탐색
+//클래스명이 js-clock인 것을 탐색
+const clockContainer = document.querySelector(".js-clock"), 
+ clockTitle =  clockContainer.querySelector("h1"); //clock 클래스의 자식을 탐색
 
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle =  clockContainer.querySelector("h1"); 
-
-//시계 생성
+//시계 생성: 시각을 가져오는 함수
 function getTime(){
-    const date = new Date();
+    const date = new Date(); //Object
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds <10 ? `0${seconds}` : seconds}`; //삼항 연산자 사용
+    //삼항 연산자
+    //(조건) ? True : False
+    //00:00:00 꼴로 만들기 위해 사용
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds <10 ? `0${seconds}` : seconds}`; 
 }
 
 function init(){
     getTime(); 
-    setInterval(getTime, 1000); //시간 간격 설정(매 초마다 업데이트)
+    setInterval(getTime, 1000); //setInterval(fn, milliseconds): 시간 간격마다 함수 실행
 }
 
 init();
--- NomadCoder/#3 JS APP/greeting.js
+++ NomadCoder/#3 JS APP/greeting.js
@@ -1,10 +1,16 @@
+//.querySelector: 찾은 첫 번째 것을 가져 옴 (클래스, 태그, 아이디)  
+//.querySelectorAll: 모든 걸 가져옴? ex. 클래스 명에 따른 엘리먼트들 > Array 형태로 반환
+//.getElementByID
+//.getElementByTagName: 태그로 엘리먼트를 가져옴 ex. input, body, html, div, section
+
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser";
-const SHOWING_ON = "showing";
+const SHOWING_CN = "showing";
 
+//저장
 function saveName(text){
     localStorage.setItem(USER_LS, text);
 }
@@ -16,25 +22,29 @@ function handleSubmit(event){
     saveName(currentValue);
 }
 
+//User의 이름을 물어보는 함수
 function askForName(){
-    form.classList.add(SHOWING_ON);
+    form.classList.add(SHOWING_CN);
     form.addEventListener("submit", handleSubmit);
 }
 
+//User에게 인사하는 함수
 function paintGreeting(text){
-    form.className.remove(SHOWING_ON);
-    greeting.className.add(SHOWING_ON);
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
     greeting.innerText =`Hello ${text}`;
 }
 
+//localStorage: 정보를 유저 컴퓨터에 저장하는 방법
+//localStorage에서 currentUser을 가져오는 함수
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
 
     if(currentUser === null){
-        // she is not
+        //저장된 값이 없으면, 이름을 물어보고
         askForName();
     } else {
-        // she is
+        //저장된 값이 있으면, 인사!
         paintGreeting(currentUser);
     }
 }
