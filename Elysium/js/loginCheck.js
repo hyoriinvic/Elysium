283fbb1fd46acbf6466ac621044e1813eac201de
Hyorin Kim <hyoriinvic@gmail.com>
2020-11-06 15:45:06 +0900

    update

--- Elysium/js/loginCheck.js
+++
@@ -1,7 +0,0 @@
-function check_login() {
-    $.ajax({
-        type: 'POST',
-        url: 'php/login.php',
-        data: "username=" + $('#username').val() + "&password=" + $('#password').val()
-    })
-}
